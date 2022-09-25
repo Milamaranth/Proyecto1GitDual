d6c23b69a720d7f0df5a01d01fe3817f0d991314
Mila <mila@example.com>
2022-09-25 10:58:39 +0200

    mensajito

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.Set;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,43 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(OwnerRepository ownerRepository, PetRepository petRepository) {
+		return args -> {
+			Owner diego = new Owner();
+			diego.setFirst_name("Diego");
+			diego.setLast_name("Pedraza");
+			diego.setAddress("Godofredo");
+			diego.setCity("Badajoz");
+			diego.setTelephone("6168336163");
+
+			Pet pet = new Pet();
+			pet.setType_id(2);
+			pet.setName("Copito");
+
+			// diego.setPets(Set.of(pet));
+
+			diego = ownerRepository.save(diego);
+			pet.setOwner(diego);
+			petRepository.save(pet);
+
+			System.out.println(ownerRepository.findAll().get(0));
+		};
+	}
+
+	/**
+	 * Crea una instancia de Pet de tipo 2 (dog) con el nombre, ownerId y birthDate que llegan por parámetro
+	 * Guarda el Pet creado en el petRepository (usa petRepository.save(...))
+	 */
+	private void creatPerro(PetRepository petRepository, String nombre, int ownerId, Date birthDate) {
+		// escribe tu código aquí
+		Pet pet = new Pet();
+		pet.setName(nombre);
+		pet.setBirth_date(birthDate);
+		pet.setType_id(2);
+		petRepository.save(pet);
+
+	}
+
 }
+//Salvador climatizacio ocaña edificio maracristina bloque 3 - 663 51 08 52
---
+++ src/main/java/com/example/demo/Owner.java
@@ -0,0 +1,77 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="owners")
+public class Owner {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+    private String first_name;
+    private String last_name;
+    private String address;
+    private String city;
+    private String telephone;
+
+    @OneToMany(mappedBy="owner", fetch = FetchType.EAGER)
+    private Set<Pet> pets;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getFirst_name() {
+        return first_name;
+    }
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    @Override
+    public String toString() {
+        return "Owner [" + first_name + " " + last_name + ", pets=" + pets + "]";
+    }
+    public Set<Pet> getPets() {
+        return pets;
+    }
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+}
