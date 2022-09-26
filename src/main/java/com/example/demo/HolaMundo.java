0a48e711c681443c7bdd27d9d9b5f7efe8b19f4e
Mila <mila@example.com>
2022-09-26 13:55:17 +0200

    rama

--- src/main/java/com/example/demo/HolaMundo.java
+++ src/main/java/com/example/demo/HolaMundo.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class HolaMundo {
 
 	public static void main(String[] args) {		
-		System.out.println("Hola Mundo");
+		System.out.println("Esta es una rama distinta");
 	}
 
 }
